d89c432e41b774043eedcbdbd2da805930751829
SJH14o3 <sajjadjh1403@gmail.com>
2023-06-08 19:38:11 +0330

    Menu now show every product that are available in database. it does use scrolling with a limit of 50 products per page. pages are not implemented yet. sort now works in 5 different ways. added some methods to help working with project.

--- src/main/java/source/Global.java
+++ src/main/java/source/Global.java
@@ -1,9 +1,19 @@
 package source;
 
+import javafx.scene.paint.Color;
+
 public class Global {
     public static final String PASSWORD = "6X0db8y3L&&J";
     public static final Integer COLOR1 = 0xE0E0E0;
     public static final Integer COLOR2 = 0xFFFF33;
+    public static Color hexToColor(int color, double opacity) {
+        if (opacity < 0.0 || opacity > 1.0) {
+            System.out.println("Invalid Opacity Value, it must be between 0 to 1, using max opacity instead");
+            opacity = 1.0f;
+        }
+        System.out.printf("%X%n", color);
+        return Color.web("#" + String.format("%X", color), opacity);
+    }
 
     private Global() {}
 }
