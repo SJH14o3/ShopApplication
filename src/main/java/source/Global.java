253f88988e82016b5e8be4f70080e2d6594fc33b
SJH14o3 <sajjadjh1403@gmail.com>
2023-06-08 09:18:33 +0330

    added a class to store global variables

---
+++ src/main/java/source/Global.java
@@ -0,0 +1,9 @@
+package source;
+
+public class Global {
+    public static final String PASSWORD = "6X0db8y3L&&J";
+    public static final Integer COLOR1 = 0xE0E0E0;
+    public static final Integer COLOR2 = 0xFFFF33;
+
+    private Global() {}
+}
