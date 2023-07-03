090d6ab1dced4bd4df3ea48c5d60bd0fa6d083e8
fatemeh <fatemeh.ebrahimi326@gmail.com>
2023-07-03 22:55:43 +0330

    payment page

--- src/main/java/source/User.java
+++ src/main/java/source/User.java
@@ -10,6 +10,9 @@ public class User {
     private static int user_type; //1: consumer, 2:vendor.
     private static double balance;
     private static String username;
+    private static String user_address;
+    private static String user_postalCode;
+    private static String user_phoneNumber;
     public static void setUser_type(int user_type) {
         User.user_type = user_type;
     }
@@ -38,5 +41,23 @@ public class User {
     }
     public static void changeBalance(double change) {
         User.balance += change;
+    }
+    public static void setUser_address(String address){
+        user_address = address;
+    }
+    public static String getUser_address(){
+        return user_address;
+    }
+    public static void setUser_postalCode(String postalCode){
+        user_postalCode = postalCode;
+    }
+    public static String getUser_postalCode() {
+        return user_postalCode;
+    }
+    public static void setUser_phoneNumber(String phoneNumber) {
+        user_phoneNumber = phoneNumber;
+    }
+    public static String getUser_phoneNumber() {
+        return user_phoneNumber;
     }
 }
