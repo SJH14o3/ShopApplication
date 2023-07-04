d953998ae4b0ddbe39e257eba2f86980bc2d970f
SJH14o3 <sajjadjh1403@gmail.com>
2023-07-04 19:52:23 +0330

    changes in DiscountCodeDataBase class

--- src/main/java/source/database/DiscountCodeDataBase.java
+++ src/main/java/source/database/DiscountCodeDataBase.java
@@ -6,17 +6,16 @@ import source.products.DatabaseConnection;
 import java.sql.*;
 
 public class DiscountCodeDataBase extends DatabaseConnection {
-    public static boolean checkCode(String code) {
-        String SQL = "SELECT count(code_id) FROM discount_codes WHERE user_id = " + Global.getUser_id() + " AND code = " + code;
+    public static double checkCode(String code) {
+        String SQL = "SELECT discount_percentage FROM discount_codes WHERE user_id = " + Global.getUser_id() + " AND code = " + code;
         try (Connection connection = establishConnection("shop"); PreparedStatement preparedStatement = connection.prepareStatement(SQL)) {
             ResultSet resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-            if (resultSet.getInt(1) > 0) {
-                return true;
+            if (resultSet.next()) {
+                return resultSet.getDouble(1);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return false;
+        return 0;
     }
 }
